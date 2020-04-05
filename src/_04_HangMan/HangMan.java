2ee7e4b76229f1362481c3b1f1ebec9be37e025c
jscornick <56415689+jscornick@users.noreply.github.com>
2020-04-05 16:01:17 -0700

    WIP HangMan, WIP TextUndoRedo
    I had to take a break from trying to figure out TextUndoRedo

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,13 @@
 package _04_HangMan;
 
+import javax.swing.JOptionPane;
+
 public class HangMan{
-	
+	public static void main(String[] args) {
+		String input = JOptionPane.showInputDialog("Give a number below 267");
+		int in = Integer.parseInt(input);
+		
+	}
 	
 	
 }
